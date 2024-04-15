83bab8e56f0e0e71a4e1dd7b6f3e8f694edaf269
Hugo Wetterberg <hugo@wetterberg.nu>
2024-04-15 09:48:04 +0200

    ELE-441 standalone factboxes (#3)
    * allow standalone factbox documents
    
    Adds UUIDs to factbox elements so that they can reference documents.
    
    * update to latest revisor version
    
    * add test case for fact box document

--- schemas_test.go
+++ schemas_test.go
@@ -2,9 +2,14 @@ package revisorschemas_test
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
+	"io/fs"
+	"os"
+	"path/filepath"
 	"testing"
 
+	"github.com/ttab/newsdoc"
 	"github.com/ttab/revisor"
 	"github.com/ttab/revisorschemas"
 )
@@ -39,6 +44,79 @@ func TestFS_ValidSchemas(t *testing.T) {
 			err = dec.Decode(&cs)
 			if err != nil {
 				t.Fatalf("failed to unmarshal schema file: %v", err)
+			}
+		})
+	}
+}
+
+func TestValidDocuments(t *testing.T) {
+	schemaFS := revisorschemas.Files()
+
+	files, err := schemaFS.ReadDir(".")
+	if err != nil {
+		t.Fatalf("failed to read directory")
+	}
+
+	var constraints []revisor.ConstraintSet
+
+	for _, f := range files {
+		data, err := schemaFS.ReadFile(f.Name())
+		if err != nil {
+			t.Fatalf("failed to read schema file: %v", err)
+		}
+
+		dec := json.NewDecoder(bytes.NewReader(data))
+
+		dec.DisallowUnknownFields()
+
+		var cs revisor.ConstraintSet
+
+		err = dec.Decode(&cs)
+		if err != nil {
+			t.Fatalf("failed to unmarshal schema file: %v", err)
+		}
+
+		constraints = append(constraints, cs)
+	}
+
+	validator, err := revisor.NewValidator(constraints...)
+	if err != nil {
+		t.Fatalf("failed to create validator with schemas: %v", err)
+	}
+
+	dataDir := os.DirFS(filepath.Join("testdata", "valid-docs"))
+
+	validDocs, err := fs.Glob(dataDir, "*.json")
+	if err != nil {
+		t.Fatalf("failed to glob for valid documents: %v", err)
+	}
+
+	for _, name := range validDocs {
+		t.Run(name, func(t *testing.T) {
+			data, err := fs.ReadFile(dataDir, name)
+			if err != nil {
+				t.Fatalf("failed to read document file: %v", err)
+			}
+
+			dec := json.NewDecoder(bytes.NewReader(data))
+
+			dec.DisallowUnknownFields()
+
+			var doc newsdoc.Document
+
+			err = dec.Decode(&doc)
+			if err != nil {
+				t.Fatalf("failed to unmarshal document file: %v", err)
+			}
+
+			validationErrors, err := validator.ValidateDocument(
+				context.Background(), &doc)
+			if err != nil {
+				t.Fatalf("validation failed: %v", err)
+			}
+
+			for _, e := range validationErrors {
+				t.Error(e.String())
 			}
 		})
 	}
